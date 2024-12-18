c07f3c4562160105b515b39f4d3a8aae90f6d820
Albi6 <169132435+Albi6@users.noreply.github.com>
2024-12-18 16:08:30 +0100

    Delete Joueur_GRAND_ACCES.sql

--- Joueur_GRAND_ACCES.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER 'joueur'@'localhost' IDENTIFIED BY 'root';
-
-GRANT SELECT ON `SpicyInvader`.`ARME` TO 'joueur'@'localhost';
-
-GRANT INSERT ON `SpicyInvader`.`COMMANDE` TO 'joueur'@'localhost';
-
-GRANT SELECT ON `SpicyInvader`.`COMMANDE` TO 'joueur'@'localhost';
-
-FLUSH PRIVILEGES;
